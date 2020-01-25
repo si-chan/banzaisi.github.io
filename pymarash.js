a7b4f675775388303dfd9a78b2147a91a48dfb93
Si Chan <38660256+banzaisi@users.noreply.github.com>
2020-01-25 16:17:30 +0000

    Update pymarash.js

--- pymarash.js
+++ pymarash.js
@@ -5,20 +5,20 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
 	ext._getStatus = function() {
-		return {status: 2, msg: "Ready"};
+		return {status: 2, msg: 'Ready'};
 	};	
 	
 	// Turn the robot arm light on
-	ext.lightOn() = function() {
+	ext.lightOn = function() {
 		$.ajax({
-			url: "http://localhost:14275/lighton/1/1"
+			url: 'http://localhost:14275/lighton/1/1'
 		});
 	};
 	
 	// Turn the robot arm light off
-	ext.lightOff() = function() {
+	ext.lightOff = function() {
 		$.ajax({
-			url: "http://localhost:14275/lightoff/1/1"
+			url: 'http://localhost:14275/lightoff/1/1'
 		});
 	};
 
@@ -26,12 +26,12 @@
 	var descriptor = {
 		blocks: [
 			// Block type, block name, function name
-			[" ", "Light ON", "lighton"],
-			[" ", "Light OFF", "lightoff"],
+			[' ', 'Light ON', 'lightOn'],
+			[' ', 'Light OFF', 'lightOff'],
 		]
 	};
 
 	// Register the extension
-	ScratchExtensions.register("RobotArm", descriptor, ext);
+	ScratchExtensions.register('RobotArm', descriptor, ext);
 	
 })({});

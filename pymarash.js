7340faa06210720259f58fa2633261b9bb4ed2cb
Si Chan <38660256+banzaisi@users.noreply.github.com>
2020-01-25 17:05:19 +0000

    Update pymarash.js

--- pymarash.js
+++ pymarash.js
@@ -1,4 +1,7 @@
 (function(ext) {
+	// URL for the server
+	const pymarashserver = 'http://localhost:14275/'
+	
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,33 +11,50 @@
 		return {status: 2, msg: 'Ready'};
 	};	
 	
-	// Turn the robot arm light on
-	ext.lightOn = function() {
+	// HTTP client function
+	function sendMessage(command, data) {
 		$.ajax({
 			type: 'GET',
-			url: 'http://localhost:14275/lighton/1/1',
+			url: 'http://localhost:14275/'+command+'/1/'+data,
 			dataType: 'html',
 			success: function(data) {}
 		});
 	};
 	
-	// Turn the robot arm light off
-	ext.lightOff = function() {
-		$.ajax({
-			type: 'GET',
-			url: 'http://localhost:14275/lightoff/1/1',
-			dataType: 'html',
-			success: function(data) {}
-		});
+	// Turn the robot arm light on
+	ext.lighton = function() {
+		sendMessage('lighton', 0);
 	};
+	
+	// Turn the robot arm light off
+	ext.lightoff = function() {
+		sendMessage('lightoff', 0);
+	};
+	
+	ext.rotatecw = function() {
+		
 
 	// Block and block menu description
 	var descriptor = {
 		blocks: [
 			// Block type, block name, function name
-			[' ', 'Light ON', 'lightOn'],
-			[' ', 'Light OFF', 'lightOff'],
-		]
+			[' ', 'Light ON', 'lighton'],
+			[' ', 'Light OFF', 'lightoff'],
+
+			["w", "Rotate clock-wise for %n second(s)", "rotatecw", 1],
+			["w", "Rotate anti clock-wise for %n second(s)", "rotateccw", 1],
+
+			["w", "Shoulder up for %n second(s)", "shoulderup", 1],
+			["w", "Shoulder down for %n second(s)", "shoulderdown", 1],
+
+			["w", "Elbow up for %n second(s)", "elbowup", 1],
+			["w", "Elbow down for %n second(s)", "elbowdown", 1],
+
+			["w", "Wrist up for %n second(s)", "wristup", 1],
+			["w", "Wrist down for %n second(s)", "wristdown", 1],
+
+			["w", "Grip close for %n second(s)", "gripopen", 1],
+			["w", "Grip open for %n second(s)", "gripclose", 1],		
 	};
 
 	// Register the extension
